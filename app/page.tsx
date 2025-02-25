85eb2494b3e8286cbfe1e90885b2fb2428f93917
Ajay <112794893+Ajay-308@users.noreply.github.com>
2025-02-25 21:40:48 +0530

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,111 @@
+"use client";
+import { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
 import Image from "next/image";
+import Link from "next/link";
+
+// on front page i have to show
+// name of the pokemon + image + type (fire, water, grass, electric, fly or rest)
+
+interface Pokemon {
+  id: number;
+  name: string;
+  image: string;
+  types: {
+    type: {
+      name: string;
+    };
+  }[]; // we have lot of type so we make an array
+  sprites: {
+    other: {
+      dream_world: {
+        front_default: string;
+      };
+    };
+  };
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [searchPokemon, setSearchPokemon] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
+        if (!res.ok) {
+          console.log("error in fetching pokemon data", res.statusText);
+        }
+        const data = await res.json();
+        if (!data) {
+          console.log("error in data ", data);
+        }
+        const pokemonData = await Promise.all(
+          data.result.map(async (pokemon: { url: string }) => {
+            const response = await fetch(pokemon.url);
+            //directly return kar raha hu to Serializing big string error aa raha ( pokemon have huge data)
+            return response.json();
+          })
+        );
+        setPokemons(pokemonData);
+        // data aa gye
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPokemons();
+  }, []);
+
+  const filteringPokemon = pokemons.filter((pokemons) => {
+    return pokemons.name.toLowerCase().includes(searchPokemon.toLowerCase());
+  });
+
+  return (
+    <div className="min-h-screen bg-gray-700 ">
+      <div className="max-w-7xl mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold text-center">Pokémon Researcher</h1>
+        <div className="relative max-w-md border-solid-white w-full mx-auto mt-6 rounded-full">
+          <FaSearch className="absolute top-3 left-3 text-gray-400" />
+          <div className="absolute top-1.9 left-8 h-10 w-0.5 bg-gray-300"></div>
+          <input
+            type="text"
+            placeholder="search your fav pokemon"
+            className="w-full pl-10 rounded-lg border border-gray-200 bg-white text-white py-2 px-2"
+            value={searchPokemon}
+            onChange={(e) => setSearchPokemon(e.target.value)}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {loading ? (
+          <div className="flex justify-center items-center h-64">
+            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
+          </div>
+        ) : (
+          <div className="grid grid-cols-1  lg:grid-col-4 md:grid-col-3 sm:grid-col-2 gap-4 mt-8">
+            {filteringPokemon.map((pokemons) => (
+              <Link
+                href={`/pokemon/${pokemons.id}`}
+                key={pokemons.id}
+                className="transform hover:scale-105 transition-transform duration-200"
+              >
+                <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden">
+                  <div className="p-4">
+                    <Image
+                      src={pokemons.sprites.other.dream_world.front_default}
+                      alt={pokemons.name}
+                      className="w-full h-48 object-contain"
+                    />
+                    <h2 className="text-xl font-semibold text-center mt-4 capitalize text-gray-800 dark:text-white">
+                      {pokemons.name}
+                    </h2>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
