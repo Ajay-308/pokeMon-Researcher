40acd41b1a0f7a0967f70ddf246caecaecf5a569
Ajay <112794893+Ajay-308@users.noreply.github.com>
2025-02-26 01:28:24 +0530

    pokemon fix

--- app/pokemon/[id]/page.tsx
+++ app/pokemon/[id]/page.tsx
@@ -76,6 +76,9 @@ export default function PokemonDetail() {
       </div>
     );
   }
+  if (!pokemon) {
+    return <div className="text-center"> pokemon not found</div>;
+  }
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50 dark:from-gray-900 dark:to-gray-800 py-8">
       <div className="max-w-4xl mx-auto px-4">
